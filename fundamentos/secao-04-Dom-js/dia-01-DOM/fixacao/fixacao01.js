d0645f12800f96773b9bc988bcebf087d5223385
João Gomes <panterudo17100@gmail.com>
2026-01-22 19:50:23 -0300

    01basicamente

---
+++ fundamentos/secao-04-Dom-js/dia-01-DOM/fixacao/fixacao01.js
@@ -0,0 +1,17 @@
+document.getElementById("title").innerText = "Star Wars: Episódio I – A Ameaça Fantasma";
+
+document.getElementsByClassName("description")[0].innerHTML = `O primeiro filme de Star Wars, "Episódio I: A Ameaça Fantasma",
+ mostra os Jedi Qui-Gon Jinn e Obi-Wan Kenobi resgatando a Rainha Amidala de Naboo de um bloqueio da Federação de Comércio,
+ descobrindo o jovem Anakin Skywalker em Tatooine e o levando para Coruscant, onde o Conselho Jedi reluta em treiná-lo,
+ culminando na morte de Qui-Gon por Darth Maul e no início da jornada de Anakin com Obi-Wan, enquanto os Sith planejam dominar a galáxia,
+ revelando uma trama política complexa`;
+
+document.getElementsByClassName("description")[0].style.fontStyle = 'italic';
+
+
+document.getElementById("sub-title").innerText = "Principais informações";
+
+document.getElementsByTagName("h2");
+// console.log(document.getElementsByTagName("h2"));
+
+
