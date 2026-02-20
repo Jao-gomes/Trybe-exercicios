d57dcaf4c85657a3dda534f1d2af8ec1c0e43304
João Gomes <panterudo17100@gmail.com>
2026-02-20 16:41:07 -0300

    tenso

---
+++ fundamentos/secao-04-Dom-js/dia-03-Eventos/praticando1/main.js
@@ -0,0 +1,91 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+const listItens = document.querySelectorAll('li');
+// const listItens2 = document.getElementsByTagName('li');
+
+
+// 1. Copie esse arquivo e edite apenas ele.
+// 1.1. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+// 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando este for clicado.
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como é possível fazer isso? Dica: Lembre-se do método `.classList.remove`.
+
+const handleChangeTech = (event) => {
+  const techElement = document.querySelector('.tech');
+  techElement.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', handleChangeTech);
+secondLi.addEventListener('click', handleChangeTech);
+thirdLi.addEventListener('click', handleChangeTech);
+
+
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech'.
+const addText = () => {
+  const techElement = document.querySelector('.tech');
+  techElement.innerHTML = input.value
+};
+// tenho que salvar numa variavel o texto que eu escrevi 
+input.addEventListener('change', addText);
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
+// redirecione para alguma página.
+// 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
+
+const doubleClick = () =>{
+  window.open('https://www.youtube.com', '_blank');
+};
+
+myWebpage.addEventListener('dblclick', doubleClick);
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo.
+
+const changeColor = (event) => {
+  event.target.style.color = 'red';
+};
+
+
+
+myWebpage.addEventListener('mouseover', changeColor);
+
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset';
+});
+
+// Segue abaixo um exemplo de uso do event.target.
+
+const resetText = (event) => {
+  // O event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na função retornará o objeto 'firstLi'.
+// console.log(listItens);
+// // console.log(listItens2);
+// console.log(myWebpage);
+
+// const testList = [...document.querySelectorAll('li')];
+
+// const arrNode = Array.from(listItens);
+// console.log(testList);
+
+// testList[1].addEventListener('click', addClassTech);
+// testList[2].addEventListener('click', addClassTech);
+
+// function addClassTech () {
+//     testList[1].classList.add('tech');
+//     testList[2].classList.add('tech');
+//}
